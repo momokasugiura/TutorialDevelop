5367f080857f40774ac2c4efa8866b622f03cb30
momoka.sugiura <momoka.sugiura.d088@dirbato.co.jp>
2024-04-25 08:58:11 +0900

    User削除

--- TutorialDevelop/src/main/java/com/techacademy/controller/UserController.java
+++ TutorialDevelop/src/main/java/com/techacademy/controller/UserController.java
@@ -1,23 +1,27 @@
 package com.techacademy.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Set; 
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute; // 追加
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping; // 追加
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam; // 追加
 
-import com.techacademy.entity.User; // 追加
+import com.techacademy.entity.User;
 import com.techacademy.service.UserService;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
-    @Autowired
-    private UserService service;
+    private final UserService service;
+    
+    public UserController(UserService service) {
+        this.service = service;
+    }
 
     /** 一覧画面を表示 */
     @GetMapping("/list")
@@ -27,8 +31,7 @@ public class UserController {
         // user/list.htmlに画面遷移
         return "user/list";
     }
-    
- // ----- 追加:ここから -----
+  
     /** User登録画面を表示 */
     @GetMapping("/register")
     public String getRegister(@ModelAttribute User user) {
@@ -45,7 +48,6 @@ public class UserController {
         return "redirect:/user/list";
     }
     
- // ----- 追加:ここから -----
     /** User更新画面を表示 */
     @GetMapping("/update/{id}/")
     public String getUser(@PathVariable("id") Integer id, Model model) {
@@ -63,5 +65,13 @@ public class UserController {
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
-    // ----- 追加:ここまで -----
+    
+    /** User削除処理 */
+    @PostMapping(path="list", params="deleteRun")
+    public String deleteRun(@RequestParam(name="idck") Set<Integer> idck, Model model) {
+        // Userを一括削除
+        service.deleteUser(idck);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
 }
--- TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
+++ TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
@@ -1,8 +1,8 @@
 package com.techacademy.service;
 
 import java.util.List;
+import java.util.Set;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,8 +11,11 @@ import com.techacademy.repository.UserRepository;
 
 @Service
 public class UserService {
-    @Autowired
     private UserRepository userRepository;
+    
+    public UserService(UserRepository repository) {
+        this.userRepository = repository;
+    }
 
     /** 全件を検索して返す */
     public List<User> getUserList() {
@@ -34,4 +37,12 @@ public class UserService {
         return userRepository.save(user);
     }
     // ----- 追加:ここまで -----
+    
+    /** Userの削除を行なう */
+    @Transactional
+    public void deleteUser(Set<Integer> idck) {
+        for(Integer id : idck) {
+            userRepository.deleteById(id);
+        }
+    }
 }
